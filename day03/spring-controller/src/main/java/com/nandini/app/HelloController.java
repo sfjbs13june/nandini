81eb770d320cf9738a97b0fbe78a11624fbc5942
nandini <nandinigajpure6@gmail.com>
2023-01-16 04:59:07 +0000

    training Day03:spring-controller

---
+++ day03/spring-controller/src/main/java/com/nandini/app/HelloController.java
@@ -0,0 +1,20 @@
+package com.nandini.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+
+    public String SayHello(){
+
+        return "Hello from App";
+    }
+    @RequestMapping("hello")
+    public String requestHello(){
+
+        return "Hello from Request Mapping";
+    }
+}
