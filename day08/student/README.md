4c72718c58e529cf433131b00849a7518f63fbd0
nandini <nandinigajpure6@gmail.com>
2023-01-24 07:30:47 +0000

    day08 docker image

--- day08/student/README.md
+++ day08/student/README.md
@@ -29,3 +29,26 @@ curl --location --request GET 'localhost:8080/get/student' \
 curl --location --request GET 'localhost:8080/map/student' \
 --header 'Cookie: JSESSIONID=EBA2CF4DBE4AEFB32DA5793D8BBB3552'
 ```
+
+## Dockerization
+
+```
+$ mvn install dockerfile:build
+```
+## List docker images
+```
+$ docker images
+```
+
+### Running the application in docker container
+
+```
+$ docker run -p 8080:8080 nandini06/student:0.0.1-SNAPSHOT
+ 
+```
+
+## List docker running container
+
+```
+$ docker ps
+```
--- day08/student/src/test/java/com/nandini/app/StudentControllerTest.java
+++ day08/student/src/test/java/com/nandini/app/StudentControllerTest.java
@@ -1,12 +1,30 @@
 package com.nandini.app;
 
+
 import com.nandini.app.controller.StudentController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class StudentControllerTest {
+import java.util.List;
+import java.util.Map;
 
+public class StudentControllerTest {
+    StudentController studentController=new StudentController();
     @Test
-    public void addTest(){
-        StudentController studentController=new StudentController();
+        public void testGetStudent(){
+        int expectedSize=3;
+        List result =studentController.getStudent();
+        Assertions.assertEquals(expectedSize,result.size());
+    }
+    @Test
+    public  void testMapStudent(){
+        int expectedSize=3;
+        Map result=studentController.mapStudent();
+        Assertions.assertEquals(expectedSize,result.size());
+    }
+
 
 }
+
+
+
