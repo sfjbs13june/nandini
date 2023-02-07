9cb67c0e78860795296146836ed30828200ef3a5
nandini <nandinigajpure6@gmail.com>
2023-02-07 08:06:16 +0000

    MiniProject

--- mini-project/hospital-management-app/src/main/java/com/nandini/app/repository/AppointmentRepository.java
+++ mini-project/hospital-management-app/src/main/java/com/nandini/app/repository/AppointmentRepository.java
@@ -1,4 +1,17 @@
 package com.nandini.app.repository;
 
-public class AppointmentRepository {
+import com.nandini.app.model.Appointment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends MongoRepository<Appointment, String> {
+
+    public Appointment findByAppointmentId(String appointmentId);
+    public Appointment findByPatientName(String patientName);
+    public Appointment findByDoctorName(String doctorName);
+    public Appointment findByDate(String date);
+    public Appointment findByPrescription(String prescription);
+    public Appointment save(Appointment appointment);
+
 }
--- mini-project/hospital-management-app/src/main/java/com/nandini/app/repository/PrescriptionRepository.java
+++ mini-project/hospital-management-app/src/main/java/com/nandini/app/repository/PrescriptionRepository.java
@@ -1,4 +1,17 @@
 package com.nandini.app.repository;
 
-public class PrescriptionRepository {
+import com.nandini.app.model.Prescription;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrescriptionRepository extends MongoRepository<Prescription, String> {
+    public Prescription findByPrescriptionId(String prescriptionId);
+    public Prescription findByAppointmentId(String appointmentId);
+    public Prescription findByDescription(String description);
+    public Prescription findByPatientName(String patientName);
+    public Prescription findByDoctorName(String doctorName);
+    public Prescription save(Prescription prescription);
+
+
 }
